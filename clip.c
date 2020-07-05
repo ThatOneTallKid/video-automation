e8c8cf01d1fcbfd8c247c6ec2855f0d88865ee64
Aditya Das <1905222@kiit.ac.in>
2020-07-05 09:59:17 +0530

    improved context.c

--- clip.c
+++ clip.c
@@ -7,4 +7,23 @@ typedef struct Clip{
 	int64_t end_time;
 	int64_t orig_start;
 	int64_t orig_end;
+	int64_t frame_index;
 }Clip;
+
+
+void init_clip_internal(Clip *clip){
+	clip->context = NULL;
+	clip->orig_start = 0;
+	clip->orig_end = -1;
+	clip->start_time = -1;
+	clip->end_time = -1;
+	clip->frame_index = 0;
+}
+
+
+
+int main(int argc, char const *argv[])
+{	clip->context->fmt_cntx = avformat_alloc_context();
+	
+	return 0;
+}
--- context.c
+++ context.c
@@ -9,31 +9,73 @@
 #include <libavutil/avutil.h>
 
 
-typedef struct VideoContext{
+
+/*typedef struct VideoContext{
 
 AVFormatContext *fmt_cntx;
 AVCodec *video;
 AVCodecContext *video_codec_cntx;
 int video_stream_id; //index of stream in fmt_cntx->streams[stream_index]
-enum PacketStreamType last_decoder_packet_stream;
+//enum PacketStreamType last_decoder_packet_stream;
 char *filename;
 int clip_cnt;
 double fps;
 int64_t curr_pts;
 int64_t seek_pts;
-}VideoContext;
+}VideoContext;*/
 
 
-void init_video(VideoContext *vid){
-	vid->fmt_cntx = NULL:
+/*void init_video(VideoContext *vid){
+	vid->fmt_cntx = NULL;
 	vid->video = NULL;
-	vid->video_codec_cntx = NULL:
+	vid->video_codec_cntx = NULL;
 	vid->video_stream_id = -1;
-	vid->last_decoder_packet_stream = DEC_STREAM_NONE;
-	vid->filename = NULL:
+	//vid->last_decoder_packet_stream = DEC_STREAM_NONE;
+	vid->filename = NULL;
 	vid->clip_cnt = 0;
 	vid->fps = 0;
 	vid->curr_pts = 0;
-	vid->seek_pts 0;
+	vid->seek_pts = 0;
+
+}*/
+
+void open_format_context(AVFormatContext *vid,char *filename){
+	char buf[256];
+	int ret = avformat_open_input(&vid,filename,NULL,NULL);
+	if(ret != 0)
+	{
+		fprintf(stderr, "ERROR: Could not open %s\n", filename);
+		av_strerror(ret, buf,256);
+		printf("%s\n",buf);
+		printf("%d\n", ret);
+		return ;
+	}
+	if(avformat_find_stream_info(vid, NULL) != 0)
+	{
+		fprintf(stderr, "Could not find stream information about the file %s\n", filename);
+		return;
+	}
+
+}
+
+void print_video_context(AVFormatContext *vid,char *filename)
+{
+	printf("format: %s\n", vid->iformat->name);
+	printf("duration: %ld\n", vid->duration);
+	printf("bit rate: %ld\n", vid->bit_rate);
+	printf("start time: %ld\n", vid->start_time);
+}
+int main(int argc, char *argv[])
+{   
+	//VideoContext *vid  ;
+	//char *filename ;
 	
+	//strcpy(filename, argv[1]);
+	//init_video(vid);
+	AVFormatContext *vid = avformat_alloc_context();
+	av_register_all();
+	open_format_context(vid, argv[1]);
+	print_video_context(vid, argv[1]);
+	
+	return 0;
 }
