26ff257c4ba78591ab2d21be5c30446e2a16d0e8
Aditya Das <1905222@kiit.ac.in>
2020-07-09 17:06:00 +0530

    added remux and demux

---
+++ demux.c
@@ -0,0 +1,15 @@
+/*
+  * Copyright (c) 2020 Aditya Das & Rupanshi Chawda
+    * @file
+    * Demux.c
+
+    * Use of the Libavformat and Libavcodec API to demux and decode only video data
+    *
+
+*/
+
+
+#include <libavutil/imgutils.h>
+#include <libavutil/samplefmt.h>
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
